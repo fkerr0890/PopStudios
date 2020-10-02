73266f71e6775b1d9d1680bfc4189701663278c8
hayleyhadges <hhadges@macalester.edu>
2020-10-02 11:58:55 -0500

    Committing & pushing input activity

--- app/src/main/java/com/example/popstudios/InputActivity.java
+++ app/src/main/java/com/example/popstudios/InputActivity.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 
 public class InputActivity extends AppCompatActivity {
     SeekBar importanceBar;
+    SeekBar difficultyBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,24 +16,39 @@ public class InputActivity extends AppCompatActivity {
         setContentView(R.layout.activity_input);
 
         importanceBar = (SeekBar)findViewById(R.id.importanceBar);
+        difficultyBar = (SeekBar)findViewById(R.id.difficultyBar);
         if (importanceBar != null) {
             importanceBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                 @Override
                 public void onProgressChanged(SeekBar importanceBar, int progress, boolean fromUser) {
-                    // Write code to perform some action when progress is changed.
                 }
 
                 @Override
                 public void onStartTrackingTouch(SeekBar importanceBar) {
-                    // Write code to perform some action when touch is started.
                 }
 
                 @Override
                 public void onStopTrackingTouch(SeekBar importanceBar) {
-                    // Write code to perform some action when touch is stopped.
-                    Toast.makeText(InputActivity.this, "Current Rating: " + importanceBar.getProgress(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(InputActivity.this, "Importance Rating: " + importanceBar.getProgress(), Toast.LENGTH_SHORT).show();
                 }
             });
         }
+        if (difficultyBar != null) {
+            difficultyBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar difficultyBar, int progress, boolean fromUser) {
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar difficultyBar) {
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar difficultyBar) {
+                    Toast.makeText(InputActivity.this, "Difficulty Rating: " + difficultyBar.getProgress(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
     }
 }
