2a06698d55f7ec0e4197b2c9d095643f4cfcd18d
hayleyhadges <hhadges@macalester.edu>
2020-10-14 12:22:25 -0500

    Added Help Page

---
+++ app/src/main/java/com/example/popstudios/FAQActivity.java
@@ -0,0 +1,14 @@
+package com.example.popstudios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FAQActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.faq_activity);
+    }
+}
