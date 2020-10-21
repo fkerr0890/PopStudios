b9667c5579ebf2ef731ae07b45c41372de9e7a25
Fred Kerr <fredk101a@gmail.com>
2020-10-21 13:02:23 -0500

    Merged Charlie's code and my code

--- app/src/main/java/com/example/popstudios/MainActivity.java
+++ app/src/main/java/com/example/popstudios/MainActivity.java
@@ -13,6 +13,8 @@ import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Rect;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -36,18 +38,19 @@ import java.util.Objects;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnLongClickListener, DeleteButtonDialog.DeleteButtonDialogListener {
-//    private static final String TAG = "MyActivity";
+    //    private static final String TAG = "MyActivity";
     ActivityMainBinding main;
     public static FeedReaderDbHelper dbHelper;
     private Animator currentAnimator;
     private int shortAnimationDuration;
     private List<Integer> listOfExpandedBubbles;
     public static float screenWidth;
+    private static float screenHeight;
     public static Map<Long,Goal> goalById;
     int deleteButtonId;
     View deleteView;
-
-    Random random;
+    private BubbleFragment bubbleFragment;
+    private static int moveCount;
 
     // Creates a new listener for when user long clicks
     View.OnLongClickListener listener = new View.OnLongClickListener() {
@@ -70,6 +73,7 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
             dialog.show(getSupportFragmentManager(), "example dialog");
         }
     };
+
 
     // setters and getters for deleting button View and ID
     public void setDeleteButtonId(int newDeleteButtonId) {
@@ -131,6 +135,7 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         db.update(FeedReaderContract.FeedEntry.TABLE_NAME, newValues,
                 FeedReaderContract.FeedEntry._ID + " = " + goal.getGoalID(),null);
         goal.setGoalStatus(1);
+        GoalListFragment.completedGoals.add((long) deleteButtonId);
         System.out.println(goal.getGoalStatus());
     }
 
@@ -143,14 +148,19 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         screenWidth = metrics.widthPixels;
+        screenHeight = metrics.heightPixels;
         shortAnimationDuration = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
         ViewPager viewPager = findViewById(R.id.pager);
         setupViewPager(viewPager);
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
-        tabLayout.getTabAt(0).setText(R.string.tab1_text);
-        tabLayout.getTabAt(1).setText(R.string.tab2_text);
+        TabLayout.Tab tab1 = tabLayout.getTabAt(0);
+        tab1.setText(R.string.tab1_text);
+        tab1.setIcon(R.mipmap.baseline_bubble_chart_white_36);
+        TabLayout.Tab tab2 = tabLayout.getTabAt(1);
+        tab2.setText(R.string.tab2_text);
+        tab2.setIcon(R.mipmap.baseline_list_white_36);
 
         dbHelper = new FeedReaderDbHelper(this);
         goalById = new HashMap<>();
@@ -227,7 +237,7 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
     public void animateBubble(View view) {
         if (currentAnimator != null)
             currentAnimator.cancel();
-
+        moveCount = 0;
         // Set the pivot point for SCALE_X and SCALE_Y transformations
         // to the top-left corner of the zoomed-in view (the default
         // is the center of the view).
@@ -255,9 +265,56 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         animate(view,scale, false);
     }
 
+    private List<View> findIntersectingBubbles(View bubble1) {
+        Rect bubble1Rect = new Rect();
+        bubble1.getHitRect(bubble1Rect);
+        List<View> result = new ArrayList<>();
+        for (View bubble2 : bubbleFragment.getBubbles()) {
+            Rect bubble2Rect = new Rect();
+            bubble2.getHitRect(bubble2Rect);
+            if (bubble1 != bubble2 && circlesIntersect(bubble1Rect.centerX(),bubble1Rect.centerY(),bubble2Rect.centerX(),bubble2Rect.centerY(),bubble1Rect.width()/2f,bubble2Rect.width()/2f, 3/(moveCount*0.20+0.5)))
+                result.add(bubble2);
+        }
+        return result;
+    }
+
+    public static boolean circlesIntersect(float x1, float y1, float x2, float y2, float r1, float r2, double amountOverlap) {
+        float distSq = (float)(Math.pow(x1 - x2,2) + Math.pow(y1 - y2,2));
+        float radSumSq = (float)Math.pow(r1 + r2,2);
+        return distSq <= radSumSq && radSumSq - distSq >= radSumSq/(amountOverlap);
+    }
+
+    private void dodgeEachOther(View bubble1, View bubble2) {
+        Rect bubble1Rect = new Rect();
+        bubble1.getHitRect(bubble1Rect);
+        Rect bubble2Rect = new Rect();
+        bubble2.getHitRect(bubble2Rect);
+        if (bubble2 != bubble1 && Rect.intersects(bubble1Rect,bubble2Rect)) {
+            float finalX;
+            float finalY;
+            float hypot = (float)Math.sqrt(Math.pow(bubble1Rect.width()/2d,2) + Math.pow(bubble1Rect.height()/2d,2)) - bubble1Rect.width()/2f;
+            float xOffset = (float) (hypot * Math.cos(Math.toRadians(45)));
+            float yOffset = (float) (hypot * Math.sin(Math.toRadians(45)));
+            if (Math.abs(bubble1Rect.left-bubble2Rect.right) < Math.abs(bubble2Rect.left-bubble1Rect.right)) {
+                finalX = bubble1Rect.left-bubble2.getWidth() + xOffset;
+            }
+            else
+                finalX = bubble1Rect.right - xOffset;
+            if (Math.abs(bubble1Rect.top-bubble2Rect.bottom) < Math.abs(bubble2Rect.top-bubble1Rect.bottom)) {
+                finalY = bubble1Rect.top-bubble2.getHeight() + yOffset;
+            }
+            else
+                finalY = bubble1Rect.bottom - yOffset;
+
+            finalX = Math.min(Math.max(0,finalX),bubbleFragment.getWidth()-bubble2.getWidth());
+            finalY = Math.min(Math.max(0,finalY),bubbleFragment.getHeight()-bubble2.getHeight());
+            animate(bubble2, finalX, finalY);
+        }
+    }
+
     // takes in a bubble, scale size and shrinks/expands bubble to that scale. Also takes in a boolean
     // for deleting, if true, calls method to remove the bubble from the screen after animating
-    private void animate(final View bubble, float finalScale, final boolean deleting) {
+    private void animate(final View bubble, final float finalScale, final boolean deleting) {
         // Construct and run the parallel animation of the four translation and
         // scale properties (SCALE_X and SCALE_Y).
         AnimatorSet set = new AnimatorSet();
@@ -277,6 +334,15 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
                     ViewGroup parentView = (ViewGroup) bubble.getParent();
                     parentView.removeView(bubble);
                 }
+                else if (finalScale != 1f) {
+                    for (View intersectingBubble : findIntersectingBubbles(bubble)) {
+                        int numBubbles = bubbleFragment.getBubbles().size();
+                        if (moveCount < numBubbles*numBubbles)
+                            dodgeEachOther(bubble,intersectingBubble);
+                        moveCount++;
+                    }
+                }
+
             }
 
             @Override
@@ -288,9 +354,32 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         currentAnimator = set;
     }
 
+    private void animate(final View bubble, float finalX, float finalY) {
+        final AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(bubble,View.X,finalX))
+                .with(ObjectAnimator.ofFloat(bubble,View.Y,finalY));
+        set.setDuration(shortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                for (View intersectingBubble : findIntersectingBubbles(bubble)) {
+                    int numBubbles = bubbleFragment.getBubbles().size();
+                    if (moveCount < numBubbles*numBubbles)
+                        dodgeEachOther(bubble,intersectingBubble);
+                    moveCount++;
+                }
+            }
+
+        });
+        set.start();
+    }
+
     private void setupViewPager(ViewPager viewPager) {
         MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        adapter.addFragment(new BubbleFragment());
+        bubbleFragment = new BubbleFragment();
+        adapter.addFragment(bubbleFragment);
         adapter.addFragment(new GoalListFragment());
         viewPager.setAdapter(adapter);
     }
